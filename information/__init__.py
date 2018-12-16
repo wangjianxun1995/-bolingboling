245498eb772d58fd7cce8be41eacff994ab302c9
wangjianxun <820735881@qq.com>
2018-12-16 21:10:36 +0800

    设置db和app

--- information/__init__.py
+++ information/__init__.py
@@ -6,16 +6,18 @@ from redis import StrictRedis
 
 from config import config
 
-
-db=None
+sr =None
+db = SQLAlchemy()
 def creat_app(config_name='off-line'):
     app=Flask(__name__)
 
 
 
     app.config.from_object(config[config_name])
-    db = SQLAlchemy(app)
+    db.init_app(app)
+
         #设置 redis数据库
+    global sr
     sr = StrictRedis(host=config[config_name].REDIS_HOSt,port=config[config_name].REDIS_PORT)
 
     #设置CSRf
