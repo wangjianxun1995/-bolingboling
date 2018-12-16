7fa6a83a7cb434dca07cd028d3e2701af00fe7fa
wangjianxun <820735881@qq.com>
2018-12-16 19:52:52 +0800

    抽取配置类

--- 配置.py
+++ 配置.py
@@ -5,24 +5,11 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import  Migrate,MigrateCommand
+from config import Config
 
 app=Flask(__name__)
 
-class Config():
-    SECRET_KEY = 'KZtgoZECnZyp/hiU49YHotf2Nv4IGYqF5I7M6K3iClzvTWYtALha9E2i7wgIK78X'
-    DEBUG = True
-    # 设置mysql数据库
-    SQLALCHEMY_DATABASE_URI ='mysql:root:mysql@127.0.0.1:3306/wang_sql'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # app.config['SQLALCHEMY_DATABASE_YRI']='mysql:root:mysql@127.0.0.1:3306/wang_sql'
-
-    REDIS_HOSt = '127.0.0.1'
-    REDIS_PORT =6379
-    SESSION_TYPE  = 'redis'
-    SESSION_REDIS = StrictRedis(host=REDIS_HOSt,port=REDIS_PORT,db = 1)
-    SESSION_USE_SIGNER =True
-    PERMANENT_SESSION_LIFETIME = 3600
 app.config.from_object(Config)
 db = SQLAlchemy(app)
     #设置 redis数据库
