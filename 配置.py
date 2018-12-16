d89ecc694ca8ab9402cb5a29dd6e473ec23b790e
wangjianxun <820735881@qq.com>
2018-12-16 17:51:53 +0800

    设置完成数据迁移

--- 配置.py
+++ 配置.py
@@ -4,6 +4,7 @@ from redis import StrictRedis,Redis # 他俩一样
 from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
+from flask_migrate import  Migrate,MigrateCommand
 
 app=Flask(__name__)
 
@@ -33,6 +34,8 @@ CSRFProtect(app)
 # 设置session
 Session(app)
 manger = Manager(app)
+Migrate(app=app,db=db)
+manger.add_command('db',MigrateCommand)
 @app.route('/')
 def world():
 
