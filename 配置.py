5b372bf283185445a2539e16d54319ba67e31e12
wangjianxun <820735881@qq.com>
2018-12-16 16:33:43 +0800

    第一次 修改

--- 配置.py
+++ 配置.py
@@ -1,7 +1,21 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 app=Flask(__name__)
 
+class Config():
+    DEBUG = True
+    SQLALCHEMY_DATABASE_YRI ='mysql:root:mysql@127.0.0.1:3306/wang_sql'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # app.config['SQLALCHEMY_DATABASE_YRI']='mysql:root:mysql@127.0.0.1:3306/wang_sql'
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+
+REDIS_HOSt = '127.0.0.1'
+REDIS_PORT =6379
+sr = StrictRedis(host=Config.REDIS_HOSt,port=Config.REDIS_PORT)
 @app.route('/')
 def world():
 
