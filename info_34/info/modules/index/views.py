5a95c263033d172788973c27f1bfe1cecf9bf662
wangjianxun <820735881@qq.com>
2018-12-18 17:07:19 +0800

    网站图标显示

--- info_34/info/modules/index/views.py
+++ info_34/info/modules/index/views.py
@@ -1,6 +1,12 @@
+from flask import current_app
+from flask import render_template
+
 from info_34.info.modules.index import index_blue
 
 @index_blue.route('/')
 def index():
+    return render_template('news/index.html')
 
-    return 'index'
+@index_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
