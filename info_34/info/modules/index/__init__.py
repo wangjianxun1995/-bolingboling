468798aaf8cac3955bb8dd2bd33448703103abff
wangjianxun <820735881@qq.com>
2018-12-18 16:33:01 +0800

    我又发布了一大坨

---
+++ info_34/info/modules/index/__init__.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+index_blue = Blueprint('index',__name__)
+
+from . import views
+
+# @index_blue.route('/')
+# def index():
+
+#     return 'index'
---
+++ info_34/manage.py
@@ -0,0 +1,27 @@
+import logging
+
+from flask import current_app
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
+from info_34.info import create_app,db
+
+# StrictRedis ,Redis是一样的
+"""
+manage.py 其实主要是让我们的工程 运行起来
+至于一些配置 和初始化的设置 我们都放到 info package 中
+info 是我们进行业务逻辑处理的文件夹
+"""
+app = create_app()
+
+#使用 Manager管理类来管理 app
+manager=Manager(app)
+
+Migrate(app=app,db=db)
+#将迁移指令添加到 manager中
+manager.add_command('db',MigrateCommand)
+
+
+
+if __name__ == '__main__':
+    # app.run()
+    manager.run()
